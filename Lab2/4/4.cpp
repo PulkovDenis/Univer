bc45448e72e0face8d1079255d18b9a0a08c835f
Pulkov <Den.pulkov@mail.ru>
2019-03-06 14:12:43 +0300

    Lab 2

---
+++ Lab2/4/4.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Введите одно целое число: ";
+    cin >> a;
+
+    cout << "Является ли данное число простым?\n";
+
+    for (b = 2; b!=a; b++) {
+        if (a % b == 0) {
+            cout << "Нет\n" << endl;
+            return 0;
+        }
+    }
+    cout << "Да\n" << endl;
+    return 0;
+}
