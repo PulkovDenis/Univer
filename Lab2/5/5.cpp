38e646207e58fa472c874d0bf9c87d7cc0bf2d3c
Pulkov <Den.pulkov@mail.ru>
2019-03-11 01:14:17 +0300

    Lab 2

--- Lab2/5/5.cpp
+++ Lab2/5/5.cpp
@@ -10,20 +10,27 @@ int main()
     int m[a];
     int i;
     int b;
+    int n = 0;
 
     cout << "Введите последовательность чисел: ";
     for( i = 0; i < a; i++) {
         cin >> m[i];
-
+    }
+    for( i = 0; i < a; i++) {
         if (m[i]!=0) {
-            for (b = 2; b*b <= m[i]; b++) {
+            for (b = 2; b <= m[i]; b++) {
                 if (m[i] % b == 0) {
-                    cout << "Не простое: " << m[i] << endl;
-                    return 0;
+                    n++;
                 }
             }
-            cout << "Простое: " << m[i] << endl;
-        } else {
+            if (n > 1) {
+                cout << "Не простое: " << m[i] << endl;
+                n = 0;
+            } else {
+                cout << "Простое: " << m[i] << endl;
+                n = 0;
+            }
+        }  else {
             break;
         }
     }
