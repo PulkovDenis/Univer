e4e9fd993b86ec6e69e3b4e90a2f2fa299274a7f
Pulkov <Den.pulkov@mail.ru>
2019-03-22 16:58:21 +0300

    lab 2

--- Lab2/3/3.cpp
+++ Lab2/3/3.cpp
@@ -1,28 +1,22 @@
-#include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int i;
-    int a = 5;
-    int m[a];
+    int a;
+    int b;
     int sum = 0;
 
-    cout << "Введите значения последовательности: ";
-    for (i = 0; i < a; i++) {
-        cin >> m[i];
-    }
-
-    for (i = 0; i < a; i++)
-        if (m[i]!=0) {
-            if (m[i] > 0) {
-                sum = sum + m[i];
-            }
-        } else {
+    for (a = 0; ; a++) {
+        cout << "Введите значение целочисленной поледовательности: ";
+        cin >> b;
+        if (b != 0 && b > 0) {
+            sum += b;
+        } else if (b == 0) {
             break;
         }
-    cout << "Сумма положительных членов целочисленной последовательноти = " << sum << endl;
+    }
+    cout << "Сумма целочисленной последовательности = " << sum << endl;
     return 0;
 }
