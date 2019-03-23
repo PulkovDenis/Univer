09e0369c980fbe4b3ccb2ace232a99c9d8ce23be
Pulkov <Den.pulkov@mail.ru>
2019-03-23 16:57:25 +0300

    lab 2

---
+++ Lab2/1/1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a;
+    int i;
+    float m[a];
+    float S = 0;
+
+    cout << "Введите длину последовательности: ";
+    cin >> a;
+    cout << "Введите значения: ";
+
+    for (i = 0; i<a; i++) {
+        cin >> m[i];
+    }
+
+    for (i = 0; i<a; i++) {
+        S = S + m[i];
+    }
+    cout << "Среднее арифметическое значение последовательности = " << S/a << endl;
+    return 0;
+}
